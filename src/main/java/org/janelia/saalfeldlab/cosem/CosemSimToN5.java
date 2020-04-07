1b9e7ac5cff39e484465021f1a084245289a19cb
bogovicj <bogovicj@janelia.hhmi.org>
2020-04-07 11:20:54 -0400

    sim to n5

--- src/main/java/org/janelia/saalfeldlab/cosem/CosemSimToN5.java
+++ src/main/java/org/janelia/saalfeldlab/cosem/CosemSimToN5.java
@@ -1,5 +1,6 @@
 package org.janelia.saalfeldlab.cosem;
 
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
 import org.janelia.saalfeldlab.n5.GzipCompression;
@@ -9,9 +10,15 @@ import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 
 import ij.IJ;
 import ij.ImagePlus;
+import net.imglib2.FinalInterval;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
+import net.imglib2.realtransform.RealViews;
+import net.imglib2.realtransform.Scale3D;
 import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.util.Util;
 import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
@@ -27,8 +34,17 @@ public class CosemSimToN5 implements Callable<Void>
 	@Option( names = { "-n", "--n5" }, required = true, description = "N5 base folder." )
 	private String n5Base;
 
+	@Option( names = { "-d", "--dataset" }, required = false, description = "Base dataset for raw data." )
+	private String datasetBase = "/volumes/raw";
+
 	@Option( names = { "-b", "--block-size" }, required = false, description = "Block size", split = "," )
 	private int[] blockSize = new int[]{ 64, 64, 64 };
+
+	@Option( names = { "--skip-iso" }, required = false, description = "Skip isotropic"  )
+	private boolean skipIso = false;
+
+	@Option( names = { "-s", "--iso-dataset" }, required = false, description = "Base dataset for isotropic data." )
+	private String isoDatasetBase = "/volumes/proc";
 
 	public static void main(String[] args)
 	{
@@ -42,7 +58,7 @@ public class CosemSimToN5 implements Callable<Void>
 		return null;
 	}
 
-	public <T extends NativeType<T>> void run() throws Exception
+	public <T extends RealType<T> & NativeType<T>> void run() throws Exception
 	{
 
 		ImagePlus imp = IJ.openImage( inputImage );
@@ -63,6 +79,7 @@ public class CosemSimToN5 implements Callable<Void>
 		
 		CosemResolution res = new CosemResolution( new double[]{ pw, ph, pd }, punit );
 
+		@SuppressWarnings("unchecked")
 		Img<T> img = (Img<T>)ImageJFunctions.wrap( imp );
 
 		N5Writer n5 = new N5FSWriter( n5Base );
@@ -73,16 +90,58 @@ public class CosemSimToN5 implements Callable<Void>
 		{
 			IntervalView<T> channelImg = Views.hyperSlice( img, 2, i );
 
-			System.out.println( "sz: " + Util.printInterval( channelImg ));
-
-			String dataset = String.format("c%d", i);
-			System.out.println( "dataset: " + dataset );
-			
+			String dataset = datasetBase + "/" + String.format( "c%d", i );
 			N5Utils.save( channelImg, n5, dataset, blockSize, compression );
 			n5.setAttribute( dataset, CosemResolution.key, res );
+			
+			if( !skipIso )
+			{
+				double[] newres = isoRes( res.dimension );
+				CosemResolution isoRes = new CosemResolution( newres, punit );
+				RandomAccessibleInterval<T> isoImg = toIsotropic( channelImg, res.dimension, newres );
+
+				String isoDataset = isoDatasetBase + "/" + String.format( "c%d", i );
+				N5Utils.save( isoImg, n5, isoDataset, blockSize, compression );
+				n5.setAttribute( isoDataset, CosemResolution.key, isoRes );
+			}
 		}
 		System.out.println("finished");
 	}
+
+	public static double[] isoRes( final double[] res )
+	{
+		double bestRes = Double.MAX_VALUE;
+		for( int i = 0; i < res.length; i++ )
+		{
+			if( res[ i ] < bestRes )
+				bestRes = res[ i ];
+		}
+		double[] isores = new double[ res.length ];
+		Arrays.fill( isores, bestRes );
+		return isores;
+	}
+
+	public static <T extends RealType<T> & NativeType<T>> RandomAccessibleInterval<T> toIsotropic(
+			final RandomAccessibleInterval<T> img,
+			final double[] oldres,
+			final double[] newres )
+	{
+		double[] factors = new double[ img.numDimensions() ];
+		long[] newSz = new long[ img.numDimensions() ];
+		for( int i = 0; i < oldres.length; i++ )
+		{
+			factors[ i ] = oldres[ i ] / newres[ i ];
+			newSz[ i ] = (long)Math.ceil( ((double)img.dimension( i )) * factors[ i ] );
+		}
+		Scale3D scale = new Scale3D( factors );
+
+		return Views.interval( Views.raster( 
+					RealViews.affine(
+						Views.interpolate( Views.extendZero( img ), new NLinearInterpolatorFactory<>()),
+						scale )),
+					new FinalInterval( newSz ));
+	}
+
 	
 	public static class CosemResolution 
 	{
